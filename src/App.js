3005e758705feb314afb146fa3260a71044a8a12
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2018-05-16 17:39:41 -0500

    add background color

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import Layout from './hoc/Layout/Layout';
+import React, { Component } from "react";
+import Layout from "./hoc/Layout/Layout";
 
 class App extends Component {
   render() {
